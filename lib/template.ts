2bc51213a0874a6f75fa36ed4239ad12416398ee
shrinha <shreysinha2002@gmail.com>
2025-10-01 22:46:35 +0530

    Initial Commit

---
+++ lib/template.ts
@@ -0,0 +1,10 @@
+// templatePaths.ts
+export const templatePaths = {
+    REACT: 'starters/react-ts',
+    NEXTJS: 'starters/nextjs',
+    EXPRESS: 'starters/express-simple',
+    VUE: 'starters/vue',
+    HONO: 'starters/hono-nodejs-starter',
+    ANGULAR: 'starters/angular',
+  };
+  
